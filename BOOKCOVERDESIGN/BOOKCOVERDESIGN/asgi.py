e5e28a5981258f9ecdc4bb703994d20fc4bea3a5
praveenkarthikvlr-source <praveenkarthikvlr@gmail.com>
2025-12-24 08:55:46 +0530

    s

---
+++ BOOKCOVERDESIGN/BOOKCOVERDESIGN/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for BOOKCOVERDESIGN project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BOOKCOVERDESIGN.settings')
+
+application = get_asgi_application()
---
+++ BOOKCOVERDESIGN/Bookcoverdesign_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BookcoverdesignAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Bookcoverdesign_app'
